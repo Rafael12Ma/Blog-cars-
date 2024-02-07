dd4018b00a26127e66a2e89ee5d50e69592cebee
Rafael12Ma <100227843+Rafael12Ma@users.noreply.github.com>
2024-02-07 16:13:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,111 @@
 import express from "express";
 import bodyParser from "body-parser";
-import {dirname} from "path";
-import { fileURLToPath } from "url";
-import { emitWarning } from "process";
-const __dirname=dirname(fileURLToPath(import.meta.url));
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "cars",
+  password: "2002",
+  port: 5432,
+});
 
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+let cars = [{ id: 1, marka: "bmw", montelo: "m3" }];
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM car ORDER BY id ASC");
+    cars = result.rows;
+
+    res.render("index.ejs", {
+      listTitle: "Today",
+      carInputs: cars,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.use(express.static("public"));
 
-app.get("/read",(req,res)=>{
-    res.render("read.ejs");
+app.get("/read", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM car ORDER BY id  ASC");
+    cars = result.rows;
+
+    res.render("read.ejs", {
+      carKind: cars,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/submit",(req,res)=>{
-   const marka= req.body["onomacar"];
-   const desc=req.body["perigrafi"];
-   res.render("read.ejs",{desc:desc,marka:marka});
-   console.log(marka,desc);
+app.post("/read", async (req, res) => {
+  const marka = req.body["onomacar"];
+  const montelo = req.body["montelo"];
+  console.log(marka, montelo);
+  //   cars.push({ marka: marka });
+
+  //   const item = req.body.newItem;
+  try {
+    await db.query("INSERT INTO car (marka,montelo) VALUES ($1,$2)", [
+      marka,
+      montelo,
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
+  res.render("read.ejs", { montelo: montelo, marka: marka });
 });
 
-app.get("/write",(req,res)=>{
-    res.render("write.ejs");
+
+
+// app.post("/submit2", async (req, res) => {
+//     const marka = req.body.;
+//     const montelo = req.body["montelo"];
+//     console.log(marka, montelo);
+//     //   cars.push({ marka: marka });
+  
+//     //   const item = req.body.newItem;
+//     try {
+//       await db.query("INSERT INTO car (marka,montelo) VALUES ($1,$2)", [
+//         marka,
+//         montelo,
+//       ]);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//     res.render("read.ejs", { montelo: montelo, marka: marka });
+//   });
+
+
+
+
+app.get("/write", (req, res) => {
+  res.render("write.ejs");
 });
 
-app.get("/bmwM3",(req,res)=>{
-    res.render("bmwM3.ejs");
+app.get("/bmwM3", (req, res) => {
+  res.render("bmwM3.ejs");
 });
-app.get("/MercedesGts",(req,res)=>{
-    res.render("MercedesGts.ejs")
-})
-
-app.get("/",(req,res)=>{
-    console.log("HELLO WORLD!")
+app.get("/MercedesGts", (req, res) => {
+  res.render("MercedesGts.ejs");
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-// Twra to mono pou thelw na entaksw ston 
-// kwdika moy kai den to exw mathei akoma 
-// einai pws dhmioyrgw asygxrwna ena neo 
-// .ejs gia na mpainei mesa to oxhma xwris
-// na xreiazetai na to dimiourgisw egw. Etsi 
-// tha einai oloklirwmeno kai to mono pou tha
-// xreiastei meta einai na kanw tin katalliles 
-// allages sto CSS
+// Twra to mono pou thelw na entaksw ston
+// kwdika moy kai den to exw mathei akoma
+// // einai pws dhmioyrgw kainouria app.get me 
+// /onoma tis eggrafes tou pinaka kai na valw 
+// ws periexomeno mesa thn perigrafi tou kathe
+// amaksiou
